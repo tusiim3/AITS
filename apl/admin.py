9ae9ea2e3fdc43a534460830cf40bd73352fefe2
kulekule2003 <okureenock@gmail.com>
2025-02-10 20:55:52 +0300

    added feature for custom complaint

--- apl/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Department
-
-admin.site.register(CustomUser)
-admin.site.register(Department)
-
-# Register your models here.
---
+++ myapp/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Simpletextarea from './components/customtext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Simpletextarea />
+  </React.StrictMode>
+)
+
