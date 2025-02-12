6cf24961fe94a12a175e3e3368398f18d5e236a4
kulekule2003 <okureenock@gmail.com>
2025-02-12 20:38:17 +0300

    added components for loging in, not yet styled

---
+++ myapp/src/components/select_logs.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Select from 'react-select'
+
+const courseunit = [
+    {value: 'ist1204', label: 'Ist1204'},
+    {value: 'csc1201', label: 'csc1201'},
+    {value: 'csc1204', label: 'csc1204'},
+    {value: 'csc1200', label: 'csc1200'},
+    {value: 'csc1202', label: 'csc1202'}
+]
+
+const complaints = [
+    {value: 'missing_marks', label: 'missing marks'},
+    {value: 'incorrect_marks', label: 'incorrect marks'},
+    {value: 'remarking', label: 'remarking'}
+]
+
+const types = [
+    {value: 'test', label: 'Test'},
+    {value: 'coursework', label: 'Coursework'},
+    {value: 'final_exam', label: 'Final Exam'}
+]
+
+export const Courseunit = () => (
+    <div>
+        <h5>Select Course Unit</h5>
+        <Select options={courseunit} />
+    </div>
+)
+
+export const Complaints  = () => (
+    <div>
+        <h5>Select complaint</h5>
+        <Select options={complaints} />
+    </div>
+    
+)
+
+export const Types = () => (
+    <div>
+        <h5>Select type</h5>
+        <Select options={types} />
+    </div>
+)
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Simpletextarea from './components/customtext';
+import {Courseunit, Complaints, Types} from './components/select_logs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Simpletextarea />
+    <Courseunit />
+    <Complaints />
+    <Types />
   </React.StrictMode>
 )
 
