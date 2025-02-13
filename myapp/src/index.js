bf01cb2d9fe85002489321310485db1eb1d3b1bf
kulekule2003 <okureenock@gmail.com>
2025-02-13 11:08:22 +0300

    new folders for the different dashboards, student dashboard,lecturer dashboard, registrar dashboard

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Simpletextarea from './components/customtext';
-import {Courseunit, Complaints, Types} from './components/select_logs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Simpletextarea />
-    <Courseunit />
-    <Complaints />
-    <Types />
+    <h1> Praise GOD</h1>
+    <p>clone repository and import for each dashboard
+    </p>
   </React.StrictMode>
 )
 
