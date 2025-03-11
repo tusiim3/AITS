101a004e4c4cd8228e9128a7fb696cd7131752b9
denistendo <ssebyaladenis18@gmail.com>
2025-03-11 13:12:34 +0300

    Added a customuser model to models.py

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('lecturer', 'Lecturer'),
+        ('registrar', 'Registrar'),
+    ]
+
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
+    YEAR_OF_STUDY_CHOICES = [
+        (1, '1st Year'),
+        (2, '2nd Year'),
+        (3, '3rd Year'),
+        (4, '4th Year'),
+        (5, '5th Year'),
+    ]
+    role =models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='male')
+    year_of_study = models.IntegerField(choices=YEAR_OF_STUDY_CHOICES, default=1)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
