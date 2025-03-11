320a5c85bcc0bb19faf01f40c6c399eb7947b5cc
denistendo <ssebyaladenis18@gmail.com>
2025-03-11 13:31:30 +0300

    Added a department model to models.py

--- my_app/models.py
+++ my_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
@@ -25,3 +26,12 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
+    
+class Department(models.Model):
+    department_name = models.CharField(max_length=50, unique=True) 
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.department_name
+
+
