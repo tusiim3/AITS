c750d31f31ea51de059cb7d726477a8e4caa96d5
kulekule2003 <okureenock@gmail.com>
2025-03-12 15:23:27 +0300

    modified the log history page

--- myapp/src/index.jsx
+++ myapp/src/index.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import StudentApp from './student_dashboard/student_dashboard';
 //import AuthenticationForms from './login_signin_dashboard/login_signup';
-import App from "./student_dashboard/log_history/log_his.jsx"
+//import App from "./student_dashboard/log_history/log_his.jsx"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 )
 
--- myapp/src/student_dashboard/log_history/log_his.jsx
+++ myapp/src/student_dashboard/log_history/log_his.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './App.module.css'; // Main app styles
+import styles from './App.module.css'; 
 import Sidebar from './components/sidebar.jsx';
 import MainContent from './components/MainContent.jsx';
 
