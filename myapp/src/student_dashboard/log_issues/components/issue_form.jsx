81bf4fe138921a8395b640c9c69c7f2fef855cb8
kulekule2003 <okureenock@gmail.com>
2025-03-13 13:13:13 +0300

    changes with the student's dashboards

---
+++ myapp/src/student_dashboard/log_issues/components/issue_form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./components.css";
+
+function Myform() {
+    return (
+        <div className="myform">
+            {/* this one contains the results of the user input */}
+            <button type="submit">submit form</button>
+        </div>
+    );
+}
+
+export default Myform
