84d8c27c5e40756f881d5f47da844c5f12d5317f
denistendo <ssebyaladenis18@gmail.com>
2025-03-13 11:23:02 +0300

    created a serializers.py file in my_app

--- my_app/serializers.py
+++ my_app/serializers.py
@@ -1,10 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser, Department, Course, Issues
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'role', 'gender', 'year_of_study']
+ 
 
 
 
