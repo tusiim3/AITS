280d41324dbcecd486af30e3252a58070c843811
denistendo <ssebyaladenis18@gmail.com>
2025-03-13 11:45:51 +0300

    Added an Issues serializer in serializers.py

--- my_app/serializers.py
+++ my_app/serializers.py
@@ -16,6 +16,16 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'course_name', 'course_code']
 
+class IssuesSerializers(serializers.ModelSerializer):
+    student = CustomUserSerializer(read_only=True)
+    lecturer = CustomUserSerializer(read_only=True)
+    academic_registrar = CustomUserSerializer(read_only=True)
+    Department = DepartmentSerializer(read_only=True)
+    course = CourseSerializer(read_only=True)
+
+    class Meta:
+        model = Issues
+        fields = ['id', 'student', 'Issue_type', 'department', 'course', 'description', 'status', 'created_at', 'lecturer', 'academic_registrar']
 
 
 
