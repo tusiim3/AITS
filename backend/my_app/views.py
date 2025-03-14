539850024305a35dc137e0ae60b49898074b7f62
denistendo <ssebyaladenis18@gmail.com>
2025-03-14 12:10:13 +0300

    imported models from models.py and serializers from serializers.py

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from .models import CustomUser, Department, Course, Issues
+from .serializers import CustomUserSerializer, DepartmentSerializer, CourseSerializer, IssuesSerializer
 
 
 # Create your views here.
