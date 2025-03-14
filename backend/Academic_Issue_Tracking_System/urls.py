054199bb184f343a028dd99c7f98755b98780e4e
denistendo <ssebyaladenis18@gmail.com>
2025-03-14 15:21:06 +0300

    Registered the URL for the DepartmentViewSet, mapping it to the endpoint '/departments/'

--- backend/Academic_Issue_Tracking_System/urls.py
+++ backend/Academic_Issue_Tracking_System/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from my_app.views import CustomUserViewSet
+from my_app.views import CustomUserViewSet, DepartmentViewSet
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
+router.register(r'departments', DepartmentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
