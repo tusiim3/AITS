61e40a37e7c90bc30d2999f8f5fe1758f3b2a1fe
denistendo <ssebyaladenis18@gmail.com>
2025-03-14 12:34:37 +0300

    Added an API viewset for CustomUser

--- backend/Academic_Issue_Tracking_System/urls.py
+++ backend/Academic_Issue_Tracking_System/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from my_app.views import CustomUserViewSet
+
+router = DefaultRouter()
+router.register(r'users', CustomUserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
