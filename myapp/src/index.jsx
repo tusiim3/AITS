758d03ed55fa9e3ec113c7353c1cd67e9f6df799
kulekule2003 <okureenock@gmail.com>
2025-03-15 19:10:29 +0300

    made the log history divs scrollable

--- myapp/src/index.jsx
+++ myapp/src/index.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 //import AuthenticationForms from './login_signin_dashboard/login_signup';
 //import App from "./student_dashboard/log_history/log_his.jsx";
 //import StudentApp from './student_dashboard/log_issues/log_issues.jsx';
-//import Issue from "./registrar_dashboard/issue_history/issue_history.jsx"
+import Issue from "./registrar_dashboard/issue_history/issue_history.jsx"
 //import Pending from './registrar_dashboard/pending_issues/pending';
 //import Notif from './registrar_dashboard/notifications/notification';
 //import Profile from './registrar_dashboard/profile/profile';
@@ -14,6 +14,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Issue />
   </React.StrictMode>
 )
 
