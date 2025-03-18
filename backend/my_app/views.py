cc472ede69176056cf5a54bc4c314fee194eda45
denistendo <ssebyaladenis18@gmail.com>
2025-03-18 15:57:32 +0300

    Implemented JWT authentication by securing views with IsAuthenticated permission class

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import CustomUser, Department, Course, Issues
 from .serializers import CustomUserSerializer, DepartmentSerializer, CourseSerializer, IssuesSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer  
+    serializer_class = DepartmentSerializer     
+    permission_classes = [IsAuthenticated]
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
-    serializer_class = CourseSerializer  
+    serializer_class = CourseSerializer 
+    permission_classes = [IsAuthenticated] 
 
 class IssuesViewSet(viewsets.ModelViewSet):
     queryset = Issues.objects.all()
     serializer_class = IssuesSerializer    
+    permission_classes = [IsAuthenticated]
