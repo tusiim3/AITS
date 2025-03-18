0d7227f1daa8b2862431b5d43ce92d0b10991173
denistendo <ssebyaladenis18@gmail.com>
2025-03-18 15:42:44 +0300

    Added JWT token obtain and refresh views to urls.py

--- backend/Academic_Issue_Tracking_System/urls.py
+++ backend/Academic_Issue_Tracking_System/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from my_app.views import CustomUserViewSet, DepartmentViewSet, CourseViewSet, IssuesViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
@@ -28,4 +29,6 @@ router.register(r'issues', IssuesViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenObtainPairView.as_view(), name='token_refresh_pair'),
 ]
