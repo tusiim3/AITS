c9c09480d5314fce14d674a3fee9de14a14fede7
denistendo <ssebyaladenis18@gmail.com>
2025-03-19 16:30:54 +0300

    added a register serializer to handle user registration

--- backend/my_app/serializers.py
+++ backend/my_app/serializers.py
@@ -3,6 +3,21 @@ from .models import CustomUser, Department, Course, Issues
 from django.contrib.auth.password_validation import validate_password
 
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.Charfield(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'student_number', 'role', 'gender', 'year_of_study', 'password', 'password2']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return data
+    
+        
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
