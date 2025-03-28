aea92c61578944f6bbfd187cab3debe69ab2d7e9
denistendo <ssebyaladenis18@gmail.com>
2025-03-28 16:31:25 +0300

    Implemented Login API view with JWT authentication

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from .models import CustomUser, Department, Course, Issues
-from .serializers import CustomUserSerializer, DepartmentSerializer, CourseSerializer, IssuesSerializer ,RegisterSerializer
+from .serializers import CustomUserSerializer, DepartmentSerializer, CourseSerializer, IssuesSerializer ,RegisterSerializer, LoginSerializer
 from .permissions import IsOwnerOrIslecturerOrRegistrar
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
@@ -36,7 +37,29 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
-    
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data["user"]
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {
+                    "message": "Login successful",
+                    "user": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email,
+                        "role": user.role,
+                        "number_type": user.number_type,
+                    },
+                    "access_token": str(refresh.access_token),
+                    "refresh_token": str(refresh)
+                },
+                status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
