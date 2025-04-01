141ec8122b262d82ebe749f51b67b231b335ff1f
denistendo <ssebyaladenis18@gmail.com>
2025-04-01 16:20:52 +0300

    Addedd a url pattern for the LogoutView in urls.py

--- backend/my_app/urls.py
+++ backend/my_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet, DepartmentViewSet, CourseViewSet, IssuesViewSet, RegisterView, LoginView
+from .views import CustomUserViewSet, DepartmentViewSet, CourseViewSet, IssuesViewSet, RegisterView, LoginView, LogoutView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
@@ -14,5 +14,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),  
     path('login/', LoginView.as_view(), name='login'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('Logout/', LogoutView.as_view(), name='Logout'), 
 ]
