d8ad5fa52869b3865d2691b960dfcbe3276151e3
denistendo <ssebyaladenis18@gmail.com>
2025-04-03 09:12:43 +0300

    Added an import for the RegistrarIssueListView in urls.py

--- backend/my_app/urls.py
+++ backend/my_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet, DepartmentViewSet, CourseViewSet, IssuesViewSet, RegisterView, LoginView, LogoutView,CreateIssueView
+from .views import CustomUserViewSet, DepartmentViewSet, CourseViewSet, IssuesViewSet, RegisterView, LoginView, LogoutView,CreateIssueView, RegistrarIssueListView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 """
