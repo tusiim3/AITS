2aaee2faa4dcfaafaccc19f7281d60b47c1dfe4b
denistendo <ssebyaladenis18@gmail.com>
2025-04-03 23:14:08 +0300

    trying to deploy

--- backend/Academic_Issue_Tracking_System/settings.py
+++ backend/Academic_Issue_Tracking_System/settings.py
@@ -149,15 +149,12 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+import dj_database_url
 import os
-import django_heroku
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    )
 }
-
-# Activate Django-Heroku settings
-django_heroku.settings(locals())
