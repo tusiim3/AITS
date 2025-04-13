0178d191e5e68fdc65eab0329457ff00d808dd84
denistendo <ssebyaladenis18@gmail.com>
2025-04-13 13:37:26 +0300

    Added something in settings.py

--- backend/Academic_Issue_Tracking_System/settings.py
+++ backend/Academic_Issue_Tracking_System/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,6 +127,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = 'static/'
 
