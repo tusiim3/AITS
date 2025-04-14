2cb73a1a824a99d8fc6dc318e73716654d2b9a9b
denistendo <ssebyaladenis18@gmail.com>
2025-04-14 16:20:12 +0300

    Added conditional validation for year_of_study in RegisterSerializer to ensure that it is only compulsory for students

--- backend/my_app/apps.py
+++ backend/my_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_app' # This is the name of your app, change it to your app's name
+    name = 'my_app' 
