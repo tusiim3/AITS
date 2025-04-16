4bd152b6830ef2c75d1652f61a767dc4c1fcb1d3
marthakukiriza <kukirizamartha@gmail.com>
2025-04-16 11:48:51 +0300

    Update ALLOWED_HOSTS and CORS settings for deployment and local development

--- backend/Academic_Issue_Tracking_System/settings.py
+++ backend/Academic_Issue_Tracking_System/settings.py
@@ -162,6 +162,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
