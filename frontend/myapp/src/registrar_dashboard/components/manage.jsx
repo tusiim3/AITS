8c9fd47e7bd5e30f1c249b56031212f50e6d81a1
kulekule2003 <okureenock@gmail.com>
2025-04-16 17:43:43 +0300

    added data fectching components for the registrar manage dashboard

--- frontend/myapp/src/registrar_dashboard/components/manage.jsx
+++ frontend/myapp/src/registrar_dashboard/components/manage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import styles from "./manage.module.css";
+import axiosInstance from "../../axioscomponent";
 
 export default function Manage() {
+    const addLecturer = async(e) => {
+        try{
+            const response = await axiosInstance.post("#")
+        } catch (error) {
+            console.error("Error adding lecturer")
+        }
+    }
+
+    const lecturerList = async(e) => {
+        try {
+            const response = await axiosInstance.get("#")
+        } catch (error) {
+            console.error("Error fetching data", error);
+        }
+    };
+
     return(
         <div className={styles.container}>
             <div>lecturers list</div>
