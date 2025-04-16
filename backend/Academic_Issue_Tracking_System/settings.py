5dc98cf923ce97ae1ea3dbcc36056693529fceb6
marthakukiriza <kukirizamartha@gmail.com>
2025-04-16 16:48:22 +0300

    Finalize email backend and CORS configurations for production and development

--- backend/Academic_Issue_Tracking_System/settings.py
+++ backend/Academic_Issue_Tracking_System/settings.py
@@ -163,6 +163,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_app_password'  # use an app password, not your real password
 
 
 CORS_ALLOW_CREDENTIALS = True
