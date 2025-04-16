f3e7d2f9fc23b66a02f2db67bfc95e5a01fb156e
marthakukiriza <kukirizamartha@gmail.com>
2025-04-16 06:45:46 +0300

    Feat: Add home view and API route to main URL configuration

--- backend/Academic_Issue_Tracking_System/urls.py
+++ backend/Academic_Issue_Tracking_System/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+   
 
-from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.contrib import admin
 
 # Simple home view
 def home(request):
