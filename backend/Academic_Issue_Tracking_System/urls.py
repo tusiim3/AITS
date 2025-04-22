915d3b86cc79ea3e8c55ac539d4b078d633e953c
marthakukiriza <kukirizamartha@gmail.com>
2025-04-22 12:17:31 +0300

    Corrected syntax errors in URL patterns and integrate dashboard, login, and logout views

--- backend/Academic_Issue_Tracking_System/urls.py
+++ backend/Academic_Issue_Tracking_System/urls.py
@@ -39,6 +39,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'))
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('dashboard', dashboard)
+    path('dashboard/', dashboard, name='dashboard'),
+    path('dashboard/send_email/', send_email),
 ]
+    
 
 
