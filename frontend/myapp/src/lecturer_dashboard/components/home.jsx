4be57aec53c44a539cccf953544950a9254b0ce1
Tusiime Mark <tusiimejohnmark@gmail.com>
2025-04-23 07:09:58 -0700

    Restructured lecturer dashboard

---
+++ frontend/myapp/src/lecturer_dashboard/components/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+
+
+
+
+
+
+    
+
+
+
+
+
+
+}
+
+export default Home;
--- frontend/myapp/src/lecturer_dashboard/lecturer_dashboard.jsx
+++ frontend/myapp/src/lecturer_dashboard/lecturer_dashboard.jsx
@@ -1,94 +1,28 @@
 import React, { useState } from 'react';
+import './lecturer_dashboard.css';
+import Home from "./components/home.jsx";
 import Pend from "./components/pending_issues.jsx";
 import His from "./components/issue_history.jsx";
 import Profile from "./components/profile.jsx";
-import './lecturer_dashboard.css';
 import axiosInstance from '../axioscomponent.jsx';
+import { FiHome,FiLogOut,FiUser, FiClock } from 'react-icons/fi';
+import { FaBug, FaUniversity,FaUserCircle } from 'react-icons/fa';
 
-function Student() {
+function Lecturer() {
   // State to track which component to render
-  const [currentView, setCurrentView] = useState('PendIssues'); // Default view is 'logForm'
-  const [clickedButton, setClickedButton] = useState('PendIssues');
-
-  // Components to display
-  const PendIssues = () => (
-    <div>
-      <Pend />
-    </div>
-  );
-
-  const IssueHistory = () => (
-    <div>
-      <His />
-    </div>
-  );
-
-  const ProfileForm = () => (
-    <div>
-      <Profile />
-    </div>
-  );
+  const [currentView, setCurrentView] = useState('Home'); // Default view is 'logForm'
+  const [clickedButton, setClickedButton] = useState('Home');
 
   const handleLogout = async (e) => {
     try {
       const response = await axiosInstance.post("/Logout/");
       console.log(response.data);
       localStorage.removeItem("token");
-      window.location.href = '/'; // Corrected this line
+      window.location.href = '/'; 
     } catch (error) {
       console.error("Error during logout:", error);
       alert("Failed to logout. Please try again.");
     }
   };
 
-  return (
-    <div>
-      <div className='left_container'>
-        <div className='circle-container'>
-          <img src='./logo/martha.jpg' alt="Logo" />
-        </div>
-
-        <button
-          className={`mybuttons ${clickedButton === 'PendIssues' ? 'clicked' : ''}`}
-          onClick={() => {
-            setCurrentView('PendIssues');
-            setClickedButton('PendIssues');
-          }}
-        >
-          Pending Issues
-        </button>
-        <button
-          className={`mybuttons ${clickedButton === 'IssueHistory' ? 'clicked' : ''}`}
-          onClick={() => {
-            setCurrentView('IssueHistory');
-            setClickedButton('IssueHistory');
-          }}
-        >
-          Issue History
-        </button>
-        <button
-          className={`mybuttons ${clickedButton === 'Profile' ? 'clicked' : ''}`}
-          onClick={() => {
-            setCurrentView('Profile');
-            setClickedButton('Profile');
-          }}
-        >
-          Profile
-        </button>
-
-        <button className='logout_button' onClick={handleLogout}>
-          Log out
-        </button>
-      </div>
-
-      <div className='right_container'>
-        {currentView === 'PendIssues' && <Pend/>}
-        {currentView === 'IssueHistory' && <His />}
-        {currentView === 'Profile' && <ProfileForm />}
-      </div>
-    </div>
-  );
-}
-
-export default Student;
-
+  
