6fe9821a253ecd8abbfb2d430ff5109f46baadbd
kulekule2003 <okureenock@gmail.com>
2025-05-13 21:20:30 +0300

    added css styles

--- frontend/myapp/src/registrar_dashboard/components/profile.jsx
+++ frontend/myapp/src/registrar_dashboard/components/profile.jsx
@@ -18,49 +18,49 @@ export default function Profile() {
   return (
     <div>
       <div className={style.profedit}>
-      <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className={style.upload__image_wrapper}>
-            <h3>profile picture</h3>
-            <button
-              style={isDragging ? { color: 'red' } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              <GrAdd className={style.add_icon}/>
-            </button>
-            &nbsp;
-            {imageList.length === 1 && (
-              <div className={style.image_preview}>
-                <img src={imageList[0].data_url} alt="Uploaded" />
-                <div className={style.image_buttons}>
-                  <button onClick={() => onImageUpdate(0)}>Update</button>
-                  <button onClick={() => onImageRemove(0)}>Remove</button>
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className={style.upload__image_wrapper}>
+              <h3>profile picture</h3>
+              <button
+                style={isDragging ? { color: 'red' } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                <GrAdd className={style.add_icon}/>
+              </button>
+              &nbsp;
+              {imageList.length === 1 && (
+                <div className={style.image_preview}>
+                  <img src={imageList[0].data_url} alt="Uploaded" />
+                  <div className={style.image_buttons}>
+                    <button onClick={() => onImageUpdate(0)}>Update</button>
+                    <button onClick={() => onImageRemove(0)}>Remove</button>
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
 
-          </div>
-        )}
-      </ImageUploading>
-      <p className={style.label}>EMAIL:</p><textarea className={style.text}></textarea>
-      <p className={style.label}>CONTACT:</p><textarea className={style.text}></textarea>
-      <button className={style.save}><FaSave size={18}/>save</button>
+            </div>
+          )}
+        </ImageUploading>
+        <p className={style.label}>EMAIL:</p><textarea className={style.text}></textarea>
+        <p className={style.label}>CONTACT:</p><textarea className={style.text}></textarea>
+        <button className={style.save}><FaSave size={18}/>save</button>
       </div>
       <div className={style.green_form}>
         <p className={style.field_label}>NAME:</p>
