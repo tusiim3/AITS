b3225a55034ae48a3f7439a69162d03169c0e3ed
denistendo <ssebyaladenis18@gmail.com>
2025-05-13 11:32:38 +0300

    Fixed the env issues

--- backend/Academic_Issue_Tracking_System/settings.py
+++ backend/Academic_Issue_Tracking_System/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
+
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
